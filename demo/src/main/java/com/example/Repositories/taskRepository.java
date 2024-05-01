9edf50a1e316092806336be6006b01a8fb8a31fd
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2024-05-02 01:34:26 +0300

    erroraya

--- demo/src/main/java/com/example/Repositories/taskRepository.java
+++ demo/src/main/java/com/example/Repositories/taskRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface taskRepository extends JpaRepository<Task, String> {
+public interface taskRepository extends JpaRepository<Task, Long> {
 
     // Optional<Task> findByName(String name);
 
