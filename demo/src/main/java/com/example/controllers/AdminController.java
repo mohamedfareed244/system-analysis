8adfc5c4ee7ed602dca16f84d813cd74de293f2e
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-10 11:47:36 +0300

    list users route and renderinhg done waiting for edit in db

--- demo/src/main/java/com/example/controllers/AdminController.java
+++ demo/src/main/java/com/example/controllers/AdminController.java
@@ -1,10 +1,15 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/Admin")
 public class AdminController {
-    
+    @GetMapping("/listuser")
+    public ModelAndView GetAllUsers(){
+        return new ModelAndView("ListUsers");
+    }
 }
