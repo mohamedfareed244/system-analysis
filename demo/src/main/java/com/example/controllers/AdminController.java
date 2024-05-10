7c919bbb7e6e3a418024001af39adfcf82b82122
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-10 15:07:03 +0300

    handle the routes to send the date by th

--- demo/src/main/java/com/example/controllers/AdminController.java
+++ demo/src/main/java/com/example/controllers/AdminController.java
@@ -1,13 +1,18 @@
 package com.example.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.repositories.AdminRepository;
+
 @RestController
 @RequestMapping("/Admin")
 public class AdminController {
+    @Autowired
+    AdminRepository adminrepo;
     @GetMapping("/listusers")
     public ModelAndView GetAllUsers(){
         return new ModelAndView("ListUsers");
@@ -15,6 +20,8 @@ public class AdminController {
 
     @GetMapping("/listadmins")
     public ModelAndView GetAllAdmins(){
-        return new ModelAndView("ListAdmins");
+        ModelAndView newmodel=new ModelAndView("ListAdmins");
+        newmodel.addObject("Admins ", adminrepo.findAll());
+        return newmodel;
     }
 }
