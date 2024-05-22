c8a51c1eef85d0cefd8db404234e3767c80faea1
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-23 00:29:26 +0300

    first function from admin testing

--- demo/src/test/java/com/example/demo/AdminControllerTest.java
+++ demo/src/test/java/com/example/demo/AdminControllerTest.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.controllers.AdminController;
 import com.example.repositories.AdminRepository;
@@ -9,6 +17,19 @@ public class AdminControllerTest {
     @Mock
     private AdminRepository adminrepo;
 
-    @Mock 
+    @InjectMocks
     private AdminController admincontroller;
+
+    private MockHttpSession session;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        session = new MockHttpSession();
+    }
+
+    @Test
+    void testAdminProfile(){
+        ModelAndView mav =this.admincontroller.ProfileDetails(session);
+        assertEquals("/admin/adminProfile", mav.getViewName());
+    }
 }
